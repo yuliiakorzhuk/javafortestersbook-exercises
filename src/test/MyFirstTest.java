26837acaddc0a110445b5ebd501390b9f52fbbc5
julko <cibylko@gmail.com>
2014-10-16 23:47:53 +0300

    1-6 chapter exercises

---
+++ src/test/MyFirstTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jts on 10/16/2014.
+ */
+public class MyFirstTest {
+    @Test
+    public void canAddTwoPlusTwo(){
+        int answer = 2+2;
+        assertEquals("2+2=4", 4, answer);
+    }
+    @Test
+    public void canSubtractTwoFromTwo(){
+        int answer = 2-2;
+        assertEquals("2-2=0", 0, answer);
+    }
+    @Test
+    public void canDivideFourOnTwo(){
+        int answer = 4/2;
+        assertEquals("4/2=2", 2, answer);
+    }
+    @Test
+    public void canMultiplyTwoOnTwo(){
+        int answer = 2*2;
+        assertEquals("2*2=4", 4, answer);
+    }
+
+}
---
+++ src/test/com/javafortesters/testwithownclasses/domainobject/TestAppEnvironmentTest.java
@@ -0,0 +1,23 @@
+package test.com.javafortesters.testwithownclasses.domainobject;
+
+
+import main.java.com.javafortesters.domainobject.TestAppEnv;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * Created by jts on 10/16/2014.
+ */
+public class TestAppEnvironmentTest {
+    @Test
+    public void canGetUrlStatically(){
+        assertEquals("Hard coded URL ", "http://192.123.0.3:67", TestAppEnv.getUrl());
+    }
+
+    @Test
+    public void canGetDomainAndPortStatically(){
+        assertEquals("Just the Domain","192.123.0.3",TestAppEnv.DOMAIN);
+        assertEquals("Just the port", "67", TestAppEnv.PORT);
+    }
+}
